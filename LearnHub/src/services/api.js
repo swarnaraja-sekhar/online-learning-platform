500b5c22d590920e448beae41c0998ecb652d56e
swarnaraja-sekhar <bhuchiki12@gmail.com>
2025-10-04 21:12:24 +0530

    deployed

--- LearnHub/src/services/api.js
+++ LearnHub/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://online-learning-platform-gv8v.onrender.com/api';
 const getToken = () => localStorage.getItem('token');
 
 // Auth
